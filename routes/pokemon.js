6f08cd0deaeed2ba81168788a22a8b7ad76063ca
NatrezC <cnatrez@gmail.com>
2020-10-25 14:25:17 -0500

    part 3 complete

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,16 +1,28 @@
 const express = require('express');
 const router = express.Router();
+const db = require('../models')
 
 // GET /pokemon - return a page with favorited Pokemon
 router.get('/', function(req, res) {
   // TODO: Get all records from the DB and render to view
-  res.send('Render a page of favorites here');
+  res.render('pokemon/index.ejs')
+  //res.send('Render a page of favorites here');
 });
 
 // POST /pokemon - receive the name of a pokemon and add it to the database
 router.post('/', function(req, res) {
   // TODO: Get form data and add a new record to DB
-  res.send(req.body);
+  db.pokemon.findOrCreate({ //Now it doesn't duplicate
+    where: {name: req.body.name},
+    defaults: {name: req.body.name}
+  })
+  .then(([created, wasCreated])=>{
+    res.send(created)
+  })
+  .catch(err =>{
+    console.log('You got an error dummy: ', err)
+  })
+  // res.send(req.body);
 });
 
 module.exports = router;
